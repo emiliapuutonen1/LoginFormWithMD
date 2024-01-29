bb1958f09c4786a81a4dfa2fda0f2d8c77e040fc
emiliapuutonen1 <t2puem00@students.oamk.fi>
2024-01-29 22:08:00 +0200

    tehtävä

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/Login';
+import { PaperProvider } from 'react-native-paper'
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <Login />
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
